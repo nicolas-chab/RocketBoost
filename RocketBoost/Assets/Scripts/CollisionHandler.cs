c869ed8ac374a55cc9bfceafb98e0a728aa19406
nicolas-chab <nicochab.2006@gmail.com>
2026-03-04 19:51:24 -0300

    almost done

---
+++ RocketBoost/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,98 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] private float crashDelay = 1.5f;
+    [SerializeField] AudioClip SuccessSFX;
+    [SerializeField] AudioClip CrashSFX;
+    [SerializeField] ParticleSystem SuccessParticles;
+    [SerializeField] ParticleSystem CrashParticles;
+    AudioSource audioSource;
+    bool isControllable = true;
+    bool isCollidable = true;
+    void Start() 
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        RespondToDebugKeys();
+    }
+   private void OnCollisionEnter(Collision other) 
+   {
+        if (!isControllable || !isCollidable) 
+            { 
+                return; 
+            }
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                
+                break;
+
+            case "Fuel":
+                Debug.Log("This thing is fuel");
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+
+  
+
+    private void StartSuccessSequence()
+    {
+        audioSource.Stop();
+        SuccessParticles.Play();
+        isControllable= false;
+        GetComponent<Movement>().enabled = false; 
+        audioSource.PlayOneShot(SuccessSFX);
+        Invoke("LoadNextLevel", crashDelay); 
+        
+    }
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    private void StartCrashSequence()
+    {
+        audioSource.Stop();
+        CrashParticles.Play();
+        isControllable = false;
+        GetComponent<Movement>().enabled = false;
+        audioSource.PlayOneShot(CrashSFX);
+        Invoke("ReloadLevel", crashDelay);
+    }
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    void RespondToDebugKeys()
+    {
+        if (Keyboard.current.lKey.wasPressedThisFrame)
+        {
+            LoadNextLevel();
+        }
+        else if (Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            isCollidable = !isCollidable;
+        }
+        
+    }
+}
--- RocketBoost/Assets/Scripts/Movement.cs
+++ RocketBoost/Assets/Scripts/Movement.cs
@@ -7,9 +7,15 @@ public class Movement : MonoBehaviour
     [SerializeField] InputAction rotation;
     [SerializeField] float ThrustStrength = 1000f;
     [SerializeField] float RotateStrength = 1000f;
+    [SerializeField] AudioClip MainEngineSFX;
+    [SerializeField] ParticleSystem MainEngineParticles;
+    [SerializeField] ParticleSystem LeftThrusterParticles;
+    [SerializeField] ParticleSystem RightThrusterParticles;
+    AudioSource audioSource;
     Rigidbody Rb;
     void Start() 
     {
+        audioSource = GetComponent<AudioSource>();
         Rb = GetComponent<Rigidbody>();
     }
     private void OnEnable() 
@@ -20,33 +26,79 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        ProcessThrust();
+        ProcessThrust();    
         ProcessRotation();
     }
 
     private void ProcessThrust()
     {
+        
         if (Thrust.IsPressed())
         {
-            Rb.AddRelativeForce(Vector3.up * ThrustStrength * Time.fixedDeltaTime);
+            StartThrusting();
         }
-        
+        else
+        {
+            StopThrusting();
+        }
+
+
     }
+
+    private void StopThrusting()
+    {
+        audioSource.Stop();
+        MainEngineParticles.Stop();
+    }
+
+    private void StartThrusting()
+    {
+        Rb.AddRelativeForce(Vector3.up * ThrustStrength * Time.fixedDeltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(MainEngineSFX);
+        }
+        MainEngineParticles.Play();
+        if (!MainEngineParticles.isPlaying)
+        {
+            MainEngineParticles.Play();
+        }
+    }
+
     private void ProcessRotation()
     {
         float rotationInput = rotation.ReadValue<float>();
+        
         if(rotationInput < 0)
         {
-            ApplyRotation(RotateStrength);
+            RotateRight();
         }
         else if(rotationInput > 0)
         {
-            ApplyRotation(-RotateStrength);
+            RotateLeft();
+
         }
+
+    }
+
+    private void RotateLeft()
+    {
+        ApplyRotation(-RotateStrength);
+        RightThrusterParticles.Stop();
+        LeftThrusterParticles.Play();
+    }
+
+    private void RotateRight()
+    {
+        ApplyRotation(RotateStrength);
+        LeftThrusterParticles.Stop();
+        RightThrusterParticles.Play();
     }
 
     private void ApplyRotation(float rotationThisFrame)
     {
+        Rb.freezeRotation = true; // freezing rotation so we can manually rotate
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        Rb.freezeRotation = false; // unfreezing rotation so the physics system can take over
     }
 }
